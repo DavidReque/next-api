e042a9493af2c41e1f723b31224aca069328e94f
David Requeno <davidrequeno52@gmail.com>
2023-04-30 16:12:23 -0600

    spinner

--- app/photos/[id]/page.jsx
+++ app/photos/[id]/page.jsx
@@ -1,3 +1,5 @@
+import '@/styles/globals.css'
+
 const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 
 async function getPhoto(id) {
@@ -14,12 +16,12 @@ export default async function Page({ params }) {
   const photo = await getPhoto(params.id);
 
   return (
-    <div className="gap-5">
+    <div className="gap-3">
       <h1 className="flex justify-center items-center text-2xl font-semibold">Photo details</h1>
-      <div className="flex flex-col justify-center items-center gap-4">
+      <div className="flex flex-col justify-center items-center gap-3">
         {photo.description && <p>{photo.description}</p>}
-        <img className="w-6/12	h-6/12 object-cover hover:opacity-75 m-8" src={photo.urls.small} alt={photo.description} />
-        <p className="">{photo.created_at}</p>
+        <img className="w-6/12	h-6/12 object-cover hover:opacity-75 m-7" src={photo.urls.small} alt={photo.description} />
+        <p>{photo.created_at}</p>
         <p>Likes ❤️: {photo.likes}</p>
         <p>Fotographer: {photo.user.name}</p>
       </div>
--- components/Poke.jsx
+++ components/Poke.jsx
@@ -1,11 +1,13 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import Loading from "./Loading";
 
 export default function Poke() {
   const apiKey = process.env.NEXT_PUBLIC_API_KEY;
   const [photos, setPhotos] = useState([]);
   const [page, setPage] = useState(1);
+  const [laod, setLaod] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
@@ -16,6 +18,7 @@ export default function Poke() {
         );
         const data = await res.json();
         setPhotos(data);
+        setLaod(true);
       } catch (error) {
         console.error(error);
       }
@@ -26,40 +29,46 @@ export default function Poke() {
 
   return (
     <div>
-      <div className="flex flex-wrap -mx-2">
-        {photos.map((photo) => (
-          <div
-            onClick={() => {
-              router.push(`/photos/${photo.id}`);
-            }}
-            className="w-full sm:w-1/2 md:w-1/3 xl:w-1/4 p-2"
-            key={photo.id}
-          >
-            <img
-              className="w-full h-64 object-cover hover:opacity-75"
-              src={photo.urls.small_s3}
-              alt={photo.alt_description}
-            />
+      {laod ? (
+        <div>
+          <div className="flex flex-wrap -mx-2">
+            {photos.map((photo) => (
+              <div
+                onClick={() => {
+                  router.push(`/photos/${photo.id}`);
+                }}
+                className="w-full sm:w-1/2 md:w-1/3 xl:w-1/4 p-2"
+                key={photo.id}
+              >
+                <img
+                  className="w-full h-64 object-cover hover:opacity-75"
+                  src={photo.urls.small_s3}
+                  alt={photo.alt_description}
+                />
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
 
-      <div className="flex justify-center items-center mt-8 gap-3">
-        {page > 1 && (
-          <button
-            className="font-normal p-1 bg-white w-24 rounded-xl text-slate-950 hover:bg-black hover:text-white hover:border"
-            onClick={() => setPage(page - 1)}
-          >
-            Previous page
-          </button>
-        )}
-        <button
-          className="font-normal p-3 bg-white w-24 rounded-xl text-slate-950 hover:bg-black hover:text-white hover:border"
-          onClick={() => setPage(page + 1)}
-        >
-          Next page
-        </button>
-      </div>
+          <div className="flex justify-center items-center mt-8 gap-3">
+            {page > 1 && (
+              <button
+                className="font-normal p-1 bg-white w-24 rounded-xl text-slate-950 hover:bg-black hover:text-white hover:border"
+                onClick={() => setPage(page - 1)}
+              >
+                Previous page
+              </button>
+            )}
+            <button
+              className="font-normal p-3 bg-white w-24 rounded-xl text-slate-950 hover:bg-black hover:text-white hover:border"
+              onClick={() => setPage(page + 1)}
+            >
+              Next page
+            </button>
+          </div>
+        </div>
+      ) : (
+        <Loading/>
+      )}
     </div>
   );
 }
