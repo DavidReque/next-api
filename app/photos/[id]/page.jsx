3ae21609d1363d670f7e8e4484c05d0841b6475a
David Requeno <davidrequeno52@gmail.com>
2023-04-30 15:30:12 -0600

    photo details styles

--- app/photos/[id]/page.jsx
+++ app/photos/[id]/page.jsx
@@ -14,11 +14,12 @@ export default async function Page({ params }) {
   const photo = await getPhoto(params.id);
 
   return (
-    <div className="flex justify-center">
-      <h1>Photo details</h1>
-      <div>
+    <div className="gap-5">
+      <h1 className="flex justify-center items-center text-2xl font-semibold">Photo details</h1>
+      <div className="flex flex-col justify-center items-center gap-4">
         {photo.description && <p>{photo.description}</p>}
-        <img src={photo.urls.small} alt={photo.description} />
+        <img className="w-6/12	h-6/12 object-cover hover:opacity-75 m-8" src={photo.urls.small} alt={photo.description} />
+        <p className="">{photo.created_at}</p>
         <p>Likes ❤️: {photo.likes}</p>
         <p>Fotographer: {photo.user.name}</p>
       </div>
